d21227f95f9bdc77f8b0b2cbeecb2fe5ba856298
ovti <svdovti@gmail.com>
2023-06-04 19:17:08 +0200

    fixed the url list

--- app/migrations/Version20230604165719.php
+++ app/migrations/Version20230604165719.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230601213956 extends AbstractMigration
+final class Version20230604165719 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- app/src/Repository/UrlVisitedRepository.php
+++ app/src/Repository/UrlVisitedRepository.php
@@ -33,19 +33,6 @@ class UrlVisitedRepository extends ServiceEntityRepository
 
         return $queryBuilder->getQuery()->getResult();
     }
-//    public function countAllVisitsForUrl(): array
-//    {
-//        $queryBuilder = $this->getOrCreateQueryBuilder()
-//            ->select(
-//                'partial url.{id, long_url, short_url, create_time, is_blocked, block_expiration}',
-//                'partial urlVisited.{id, visit_time}',
-//            )
-//            ->leftJoin('urlVisited.url', 'url')
-//            ->orderBy('urlVisited.visit_time', 'DESC');
-//
-//        return $queryBuilder->getQuery()->getResult();
-//    }
-
 
 
     private function getOrCreateQueryBuilder(QueryBuilder $queryBuilder = null): QueryBuilder
@@ -53,47 +40,4 @@ class UrlVisitedRepository extends ServiceEntityRepository
         return $queryBuilder ?? $this->createQueryBuilder('urlVisited');
     }
 
-
-    public function save(UrlVisited $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->persist($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-    public function remove(UrlVisited $entity, bool $flush = false): void
-    {
-        $this->getEntityManager()->remove($entity);
-
-        if ($flush) {
-            $this->getEntityManager()->flush();
-        }
-    }
-
-//    /**
-//     * @return UrlVisited[] Returns an array of UrlVisited objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?UrlVisited
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
